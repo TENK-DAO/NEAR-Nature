2318015597ada8bdf693305ea41900127d0e6241
rickknowlton <richard.d.knowlton@gmail.com>
2022-06-17 01:31:21 -0400

    v 1.1.1

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,86 +5,110 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-
-type MetaTag =
-  | {
-      name: string
-      content: string
-    }
-  | {
-      property: string
-      content: string
-    }
-
-interface Props {
-  description: string
-  lang: string
-  title: string
-  meta?: MetaTag[]
-}
-
-function Seo({ description, lang, meta = [], title }: Props) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            author
-            siteUrl
-          }
-        }
-      }
-    `
-  )
-
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:url`,
-          content: site.siteMetadata?.siteUrl || ``,
-        },
-        {
-          property: `og:description`,
-          content: description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: description,
-        },
-      ].concat(meta)}
-    />
-  )
-}
-export default Seo
+ import * as React from "react"
+ import { Helmet } from "react-helmet"
+ import { useStaticQuery, graphql } from "gatsby"
+ import settings from "../../config/settings.json"
+ 
+ type MetaTag =
+   | {
+       name: string
+       content: string
+     }
+   | {
+       property: string
+       content: string
+     }
+ 
+ interface Props {
+   description: string
+   lang: string
+   title: string
+   meta?: MetaTag[]
+   favicon?: string
+   image?: string
+ }
+ 
+ function Seo({
+   description,
+   lang,
+   meta = [],
+   title,
+   favicon,
+   image,
+ }: Props) {
+   const { site } = useStaticQuery(
+     graphql`
+       query {
+         site {
+           siteMetadata {
+             author
+             siteUrl
+           }
+         }
+       }
+     `
+   )
+ 
+   return (
+     <Helmet
+       htmlAttributes={{
+         lang,
+       }}
+       title={title}
+       link={
+         favicon
+           ? [{ href: favicon, rel: 'icon', type: 'image/png' }]
+           : undefined
+       }
+       meta={[
+         {
+           name: `description`,
+           content: description,
+         },
+         {
+           property: `og:title`,
+           content: title,
+         },
+         {
+           property: `og:url`,
+           content: site.siteMetadata?.siteUrl || ``,
+         },
+         {
+           property: `og:description`,
+           content: description,
+         },
+         {
+           property: `og:image`,
+           content: !image ? '' : new URL(image, settings.siteUrl).href,
+         },
+         {
+           property: `og:type`,
+           content: `website`,
+         },
+         {
+           name: `twitter:card`,
+           content: `summary_large_image`,
+         },
+         {
+           name: `twitter:creator`,
+           content: site.siteMetadata?.author || ``,
+         },
+         {
+           name: `twitter:title`,
+           content: title,
+         },
+         {
+           name: `twitter:description`,
+           content: description,
+         },
+         {
+           name: `twitter:image`,
+           content: !image ? '' : new URL(image, settings.siteUrl).href,
+         }
+       ].concat(meta)}
+     />
+   )
+ }
+ export default Seo
+ 
